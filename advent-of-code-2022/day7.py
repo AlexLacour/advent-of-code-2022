3608b0e8448a786a6432006ce0fae9e90c06c3c2
AlexLacour <alexandre.lacour@xxii.fr>
2022-12-07 12:51:48 +0100

    day 7

--- advent-of-code-2022/day7.py
+++ advent-of-code-2022/day7.py
@@ -7,9 +7,10 @@ from collections import defaultdict
 raw_input = get_input(day=7)
 
 
-def get_file_system_architecture() -> dict:
+def get_file_system_architecture():
     current_dir = []
     system = {}
+
     for line_id, line in enumerate(raw_input):
         split_line = line.split()
         if split_line[0] == "$":  # instruction
@@ -18,39 +19,54 @@ def get_file_system_architecture() -> dict:
                     current_dir.pop()
                 else:
                     current_dir.append(split_line[-1])
-                    if split_line[-1] not in system:
-                        system[split_line[-1]] = {}
+                    system_subdict = system
+                    for dir_name in current_dir[:-1]:
+                        system_subdict = system_subdict[dir_name]
+                    if current_dir[-1] not in system_subdict:
+                        system_subdict[current_dir[-1]] = {}
             elif split_line[1] == "ls":
                 for ls_line in raw_input[line_id + 1:]:
                     if ls_line.startswith("$"):
                         break
                     split_ls_line = ls_line.split()
-                    if ls_line.startswith("dir"):
-                        system[split_ls_line[-1]] = {}
-                    else:
-                        system[current_dir[-1]][split_ls_line[1]] = int(split_ls_line[0])
-                        
+                    if not ls_line.startswith("dir"):
+                        system_subdict = system
+                        for dir_name in current_dir[:-1]:
+                            system_subdict = system_subdict[dir_name]
+                        system_subdict[current_dir[-1]][split_ls_line[1]] = int(split_ls_line[0])
+
     return system
 
 
-def folder_size(system_dict: dict) -> dict:
-    folder_size_int = 0
-    for key, value in system_dict.items():
-        if isinstance(value, dict):
-            folder_size_int += folder_size(value)
-            return folder_size_int
-        else:
-            return value
+def get_folder_size(system_dict, folder_size_memory):
+    size = 0
+    for sub_name, content in system_dict.items():
+        size += content if isinstance(content, int) else get_folder_size(content, folder_size_memory)
+
+    folder_size_memory.append(size)
+
+    return size
 
 
 def part1():
     system = get_file_system_architecture()
-    
-    return folder_size(system)
+
+    folder_size = []
+    _ = get_folder_size(system, folder_size)
+
+    return sum([value for value in folder_size if value <= 100000])
 
 
 def part2():
-    pass
+    system = get_file_system_architecture()
+
+    folder_size = []
+    total = get_folder_size(system, folder_size)
+
+    unused_space = 70_000_000 - total
+    needed_space = 30_000_000 - unused_space
+
+    return min([value for value in folder_size if value >= needed_space])
 
 
 if __name__ == "__main__":
